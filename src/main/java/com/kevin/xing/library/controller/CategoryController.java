778d827275454a849324966b16251f776052712f
Kevin Xing <2064406617@qq.com>
2021-09-27 18:03:12 +0800

    add category

---
+++ src/main/java/com/kevin/xing/library/controller/CategoryController.java
@@ -0,0 +1,42 @@
+package com.kevin.xing.library.controller;
+
+import com.kevin.xing.library.model.Category;
+import com.kevin.xing.library.service.CategoryService;
+import com.kevin.xing.result.Result;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Resource
+    private CategoryService categoryService;
+
+    @RequestMapping(value = "/findList")
+    @ResponseBody
+    public Result findList(){
+        try {
+            List<Category> list = categoryService.findList();
+            return new Result(list);
+        } catch (Exception e) {
+            return new Result(e.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/findInfo")
+    @ResponseBody
+    public Result findInfo(@RequestBody Category category){
+        try {
+            Category info = categoryService.findInfo(category.getId());
+            return new Result(info);
+        } catch (Exception e) {
+            return new Result(e.getMessage());
+        }
+    }
+}
